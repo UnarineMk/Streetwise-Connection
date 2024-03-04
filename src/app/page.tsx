3e0e813b137dd891705901171ab73dc91a194260
Unarine.Makhesha <unarine.makhesha@dotdigital.com>
2024-03-05 00:14:11 +0200

    New Project start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,6 +37,9 @@ export default function Home() {
           height={37}
           priority
         />
+          <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
